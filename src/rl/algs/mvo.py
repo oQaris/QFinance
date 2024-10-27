eb5bd91b728d49437a9fc1c99169b346a41e6cdf
oQaris <romanovomsk2014@gmail.com>
2024-10-27 23:20:03 +0600

    Добавлены методы бектеста модели

---
+++ src/rl/algs/mvo.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+from pypfopt import expected_returns, BlackLittermanModel
+from pypfopt import risk_models
+from pypfopt.efficient_frontier import EfficientFrontier
+
+from src.rl.trainer import load_dataset
+
+
+def modern_portfolio_theory(df):
+    df_stocks = _prepare_df(df)
+
+    # Годовая доходность
+    mu = expected_returns.mean_historical_return(df_stocks)
+    # Дисперсия портфеля
+    # sigma = risk_models.sample_cov(df_stocks)
+    sigma = risk_models.CovarianceShrinkage(df_stocks).ledoit_wolf()
+
+    # Максимизируем коэффициент Шарпа
+    ef = EfficientFrontier(mu, sigma, weight_bounds=(0, 1))  # (-1,1), если допустимо торговать в шорт
+    ef.max_sharpe()
+    sharpe_pwt = ef.clean_weights()
+    print(sharpe_pwt)
+    ef.portfolio_performance(verbose=True)
+
+    columns = list(df_stocks.columns)
+    result = np.zeros(len(columns), dtype=float)
+    for index, key in enumerate(columns):
+        result[index] = sharpe_pwt[key]
+    return result
+
+
+def black_litterman_theory(df):
+    df_stocks = _prepare_df(df)
+
+    # 1. Рассчитаем ковариацию активов и среднюю историческую доходность
+    mu = expected_returns.mean_historical_return(df_stocks)
+    sigma = risk_models.CovarianceShrinkage(df_stocks).ledoit_wolf()
+
+    # 2. Создаем модель Блэка-Литтермана
+    # todo Пустые матрицы для Q и P, их надо эвристически заполнить. Сейчас функция аналогична modern_portfolio_theory()
+    q = np.array([])
+    p = np.zeros((0, mu.shape[0]))
+
+    bl = BlackLittermanModel(sigma, pi=mu, P=p, Q=q)
+
+    # 3. Получаем скорректированные доходности
+    ret_bl = bl.bl_returns()
+
+    # 4. Оптимизация портфеля
+    ef = EfficientFrontier(ret_bl, sigma)
+    ef.max_sharpe()
+    sharpe_pwt = ef.clean_weights()
+    # print(sharpe_pwt)
+    ef.portfolio_performance(verbose=True)
+
+    columns = list(df_stocks.columns)
+    result = np.zeros(len(columns), dtype=float)
+    for index, key in enumerate(columns):
+        result[index] = sharpe_pwt[key]
+    return result
+
+
+def _prepare_df(df):
+    df_stocks = df.copy()
+    df_stocks['price'] = df_stocks['price'] * df_stocks['lot']
+    df_stocks = df_stocks.pivot_table(
+        index='date',
+        columns='tic',
+        values='price'
+    )
+    df_stocks['RUB'] = 1
+    if df_stocks.isna().sum().sum() > 0:
+        raise ValueError('NaN in dataset')
+    return df_stocks
+
+
+def top_stocks(df, n: int, skip_first: int = 0):
+    df = df.copy()
+    df['date'] = pd.to_datetime(df['date'])
+
+    df = df.sort_values(['tic', 'date'])
+    # пропускаем первые skip_first записей в каждом tic
+    df = df.groupby('tic').apply(lambda x: x.iloc[skip_first:]).reset_index(drop=True)
+
+    # Находим первый и последний закрывающий курс для каждой акции
+    grouped = df.groupby('tic').agg(first_price=('price', 'first'), last_price=('price', 'last'))
+
+    # Вычисляем процентное изменение для каждой акции
+    grouped['percentage_change'] = (grouped['last_price'] - grouped['first_price']) / grouped['first_price']
+
+    # Находим топ акций по процентному изменению
+    top_n_stocks = grouped.nlargest(n, 'percentage_change')
+    print(top_n_stocks)
+
+    unique_tics = pd.Series(np.concatenate((['RUB'], df['tic'].unique())))
+    result = np.where(unique_tics.isin(top_n_stocks.index), 1. / n, 0.)
+    return result
+
+
+if __name__ == '__main__':
+    _, trade = load_dataset()
+
+    # profiler = LineProfiler()
+    # profiler.add_function(top_stocks)
+    # profiler_wrapper = profiler(top_stocks)
+    modern_portfolio_theory(trade)
+    print()
+    black_litterman_theory(trade)
+    # profiler.print_stats()
--- src/rl/tester.py
+++ src/rl/tester.py
@@ -1,46 +1,94 @@
+from typing import Callable
+
 import matplotlib.pyplot as plt
-from line_profiler import LineProfiler
-from stable_baselines3 import PPO
-from stable_baselines3.common.base_class import BaseAlgorithm
+import numpy as np
+import pandas as pd
+import torch
 
-from src.rl.env import PortfolioOptimizationEnv, distribute_optimally
-from src.rl.trainer import load_dataset, build_env
+from src.rl.algs.mvo import modern_portfolio_theory, top_stocks
+from src.rl.env import PortfolioOptimizationEnv
+from src.rl.loaders import get_start_end_dates
+from src.rl.models import PolicyGradient
+from src.rl.nets_zoo import EI3
+from src.rl.trainer import load_dataset, build_env, time_window
 
 
-def prediction(model: BaseAlgorithm, env: PortfolioOptimizationEnv, deterministic=True):
+def validation(predict_fn: Callable[[pd.DataFrame], np.ndarray], env: PortfolioOptimizationEnv):
     test_obs, _ = env.reset()
     while True:
-        action, _states = model.predict(test_obs, deterministic=deterministic)
+        action = predict_fn(test_obs)
         test_obs, _, dones, _, _ = env.step(action)
         if dones:
             break
     return env.get_portfolio_size_history()
 
 
+def validation_hold(predict_fn: Callable[[pd.DataFrame], np.ndarray], env: PortfolioOptimizationEnv):
+    test_obs, _ = env.reset()
+    action = predict_fn(test_obs)
+    while True:
+        test_obs, _, dones, _, info = env.step(action)
+        action = info['real_weights']
+        if dones:
+            break
+    return env.get_portfolio_size_history()
+
+
 def backtest():
-    _, trade = load_dataset()
-    env_trade = build_env(trade)
-    trained_model = PPO.load('trained_models/agent_ppo')
+    train, trade = load_dataset()
+    env_trade = build_env(trade, verbose=2)
+    print(get_start_end_dates(trade))
 
-    profiler = LineProfiler()
-    profiler.add_function(prediction)
-    profiler.add_function(env_trade.step)
-    profiler.add_function(env_trade._rebalance_portfolio)
-    profiler.add_function(env_trade.calc_fee_portfolio)
-    profiler.add_function(env_trade._get_state_and_info_from_time_index)
-    profiler.add_function(distribute_optimally)
+    # trained_model = PPO.load('trained_models/agent_ppo')
+    # model_predict_fun = lambda obs: trained_model.predict(obs, deterministic=True)[0]
 
-    profiler_wrapper = profiler(prediction)
-    account_value = profiler_wrapper(trained_model, env_trade, deterministic=False)
+    policy = EI3(time_window=time_window, initial_features=len(env_trade._window_features))
+    policy.load_state_dict(torch.load('trained_models/policy_EI3.pt'))
+    PolicyGradient(env_trade).test(env_trade, online_training_period=999990, policy=policy)
 
-    plt.plot(account_value, label="PPO model")
-    plt.xlabel("Days")
+    mvo_result = modern_portfolio_theory(train)  # Обучаемся на train
+    mvo_fun = lambda obs: mvo_result
+
+    portfolio_size = trade['tic'].nunique()
+    ubah_fun = lambda obs: [0] + [1 / portfolio_size] * portfolio_size
+
+    # Тут именно берём топ акций, которые вырастут на trade,
+    # отсекаем первое окно, поскольку оно не участвует в сравнении
+    top_5_stocks = top_stocks(trade, 5, skip_first=time_window)
+    top_5_fun = lambda obs: top_5_stocks
+
+    top_1_stocks = top_stocks(trade, 1, skip_first=time_window)
+    top_1_fun = lambda obs: top_1_stocks
+
+    # profiler = LineProfiler()
+    # profiler.add_function(validation)
+    # profiler.add_function(env_trade.step)
+    # profiler.add_function(env_trade._rebalance_portfolio)
+    # profiler.add_function(env_trade._allocate_with_fee)
+    # profiler.add_function(env_trade._get_state_and_info_from_time_index)
+    # profiler.add_function(discrete_allocation_custom)
+    # profiler.add_function(RNNPolicyNetwork.forward)
+    #
+    # profiler_wrapper = profiler(validation)
+    # account_value_model = profiler_wrapper(model_predict_fun, env_trade)
+    account_value_model = env_trade.get_portfolio_size_history()
+    account_value_mvo = validation_hold(mvo_fun, env_trade)
+    account_value_ubah = validation_hold(ubah_fun, env_trade)
+    account_value_top_5 = validation_hold(top_5_fun, env_trade)
+    account_value_top_1 = validation_hold(top_1_fun, env_trade)
+
+    plt.plot(account_value_model, label="PPO model")
+    plt.plot(account_value_mvo, label="MVO")
+    plt.plot(account_value_ubah, label="UBAH")
+    plt.plot(account_value_top_5, label="TOP_5")
+    plt.plot(account_value_top_1, label="TOP_1")
+    plt.xlabel("Times")
     plt.ylabel("Portfolio Value")
     plt.title("Performance")
     plt.legend()
     plt.show()
 
-    profiler.print_stats()
+    # profiler.print_stats()
 
 
 if __name__ == '__main__':
